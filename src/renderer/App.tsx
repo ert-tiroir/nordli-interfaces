d58e6b51d96d0f05d893e0fbd4cdc69b8e49485a
theo.hollender <theo.hollender@gmail.com>
2024-07-22 12:07:54 +0200

    1.x: initial setup

---
+++ src/renderer/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(): React.JSX.Element {
+  return (
+    <div>
+      <h1 className="bg-gray-500 text-white text-2xl">💖 Hello World!</h1>
+      <p>Welcome to Nordli Interfaces !</p>
+    </div>
+  );
+}
+
+export default App;
